c60817a3bb210b958ab29af14e60976550fcce2e
Elia Tolin <eliatolin@gmail.com>
2022-11-24 00:36:03 +0100

    add error code

--- models/exceptions/amazon_exception.py
+++ models/exceptions/amazon_exception.py
@@ -1,39 +1,39 @@
 from models.exceptions.base_exception import Error
-
+import constant.error_code_message_constants as error_message
 
 class InvalidArgumentAmazonException(Error):
     """The value provided in the request for atleast one parameter is invalid."""
-    code_message = "invalid_arguments"
+    code_message = error_message.invalid_arguments
     pass
 
 
 class CategoryNotExistException(Error):
     """Category not exists"""
-    code_message = "category_not_exist"
+    code_message = error_message.category_not_exist
     pass
 
 
 class UrlNotDefinedAmazonException(Error):
     """Url not defined """
-    code_message = "url_not_defined"
+    code_message = error_message.url_not_definited
     pass
 
 
 class MissingParameterAmazonException(Error):
     """At least one of Actor, Artist, Author,
     Brand, BrowseNodeId, Keywords, SearchIndex, Title should be provided."""
-    code_message = "missing_parameter"
+    code_message = error_message.missing_parameter
     pass
 
 
 class GenericErrorAmazonException(Error):
     """Generic Error in Amazon API"""
-    code_message = "generic_error_amazon"
+    code_message = error_message.generic_error_amazon
     pass
 
 
 class TooManyRequestAmazonException(Error):
     """except amazon_paapi.errors.exceptions.TooManyRequests:
         https://github.com/sergioteula/python-amazon-paapi/discussions/59"""
-    code_message = "too_many_request"
+    code_message = error_message.too_many_request
     pass
