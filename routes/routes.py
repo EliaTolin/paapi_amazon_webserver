9b259b4720cf100ec9b33517ace71c28fafaa015
EliaTolin <60351315+EliaTolin@users.noreply.github.com>
2022-11-24 18:45:49 +0100

    Bug fixxing

--- routes/routes.py
+++ routes/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from amazon_routes import amazon_route
-from stats_routes import stats_route
+from routes.amazon_routes import amazon_route
+from routes.stats_routes import stats_route
+
 
 base_api = Blueprint('base_api_v1', __name__, url_prefix='/api/v1/')
 
