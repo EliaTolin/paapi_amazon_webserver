701dc546e218a1506c1054071cb0b14d7820ce99
Elia Tolin <eliatolin@gmail.com>
2022-12-10 15:12:34 +0100

    dev docker-compose development

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python
 WORKDIR /code
 
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import config
 import init_server as init
 from services.factory_services import *
 
+flask_app = init_services()
+ 
 if __name__ == '__main__':
-    if init.init_server():
-        flask_app = init_services()
+    if init.init_server():   
         flask_app.run(host='0.0.0.0', debug=config.DEBUG)
     else:
         print("###### THERE WAS AN ERROR, CHECK THE LOGS ######")
