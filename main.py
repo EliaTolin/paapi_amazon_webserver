e7c43663066f99755537b7ed8a26707a3fee6cd4
Elia Tolin <eliatolin@gmail.com>
2022-12-28 21:50:43 +0100

    Remove flask start

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ if __name__ == '__main__':
         if not init.init_server():
             print("###### THERE WAS AN ERROR, CHECK THE LOGS ######")
             exit(-1)
-        flask_app.run(host="0.0.0.0")
-        
+
     except Exception as e:
         dbg_message.show_message_debug(message="EXCEPTION CAPTURED IN MAIN",
                                        type_message=dbg_message.TypeMessage.ERROR)
